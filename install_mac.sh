69e6fe3a1a4602526806ecd69b193d6c83ad9d8e
Ruud Welten <ruudwelten@gmail.com>
2020-05-30 00:26:11 +0200

    Create Atom install function

--- install_mac.sh
+++ install_mac.sh
@@ -22,10 +22,26 @@ function install_node {
     fi
 }
 
+function install_atom {
+    which -s atom
+    if [[ $? != 0 ]]; then
+        log "Installing Atom"
+        brew install atom
+    else
+        log "Upgrading Atom"
+        brew upgrade atom
+    fi
+}
+
 log "Detected macOS"
 
 continue_skip_exit "Install Homebrew and Node?"
 if [ $? = 0 ]; then
     install_homebrew
     install_node
+fi
+
+continue_skip_exit "Install Atom and its packages?"
+if [ $? = 0 ]; then
+    install_atom
 fi
