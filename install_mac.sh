c05d9f02328f64751d369167da7512291536c32a
Ruud Welten <ruudwelten@gmail.com>
2020-05-31 23:33:17 +0200

    Remove install functions from install_mac.sh
    These functions are used only once and only make the file less readable.
    Removing them also makes the flow more linear.

--- install_mac.sh
+++ install_mac.sh
@@ -1,40 +1,5 @@
 #!/usr/bin/env bash
 
-function install_homebrew {
-    which -s brew
-    if [[ $? != 0 ]]; then
-        log "Installing Homebrew"
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-    else
-        log "Updating Homebrew"
-        brew update
-    fi
-}
-
-function install_node {
-    which -s node
-    if [[ $? != 0 ]]; then
-        log "Installing Node"
-        brew install node
-    else
-        log "Upgrading Node"
-        brew upgrade node
-    fi
-}
-
-function install_atom {
-    which -s atom
-    if [[ $? != 0 ]]; then
-        log "Installing Atom"
-        brew install atom
-    else
-        log "Upgrading Atom"
-        brew upgrade atom
-    fi
-}
-
-log "Detected macOS"
-
 # Source .dotfiles/.bash_profile from ~/.bash_profile
 if ! tail -n 1 ~/.bash_profile | grep -Fxq "source ${DIR}/.bash_profile"; then
     log "Source ${DIR}/.bash_profile from ~/.bash_profile"
@@ -44,13 +9,37 @@ if ! tail -n 1 ~/.bash_profile | grep -Fxq "source ${DIR}/.bash_profile"; then
     printf "\nsource ${DIR}/.bash_profile\n" >> ~/.bash_profile
 fi
 
-continue_skip_exit "Install Homebrew and Node?"
+continue_skip_exit "Install or update Homebrew and Node?"
 if [ $? = 0 ]; then
-    install_homebrew
-    install_node
+    # Homebrew
+    which -s brew
+    if [[ $? != 0 ]]; then
+        log "Installing Homebrew"
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    else
+        log "Updating Homebrew"
+        brew update
+    fi
+
+    # Node
+    which -s node
+    if [[ $? != 0 ]]; then
+        log "Installing Node"
+        brew install node
+    else
+        log "Upgrading Node"
+        brew upgrade node
+    fi
 fi
 
-continue_skip_exit "Install Atom and its packages?"
+continue_skip_exit "Install or upgrade Atom and its packages?"
 if [ $? = 0 ]; then
-    install_atom
+    which -s atom
+    if [[ $? != 0 ]]; then
+        log "Installing Atom"
+        brew install atom
+    else
+        log "Upgrading Atom"
+        brew upgrade atom
+    fi
 fi
